a3792688a7b0fcadeebf275e045f8f9ebcf95405
voodooppl <romeo_oprescu@yahoo.com>
2024-07-02 15:48:13 +0300

    Initial commit to new repository

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(DataContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsersAsync()
+    {
+        return await context.Users.ToListAsync();
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUsersAsync(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+
+        if (user == null) return NotFound();
+
+        return user;
+    }
+}
